1d6c45852760a5697f84be1c1b40d4f7fe8d0140
bernovie <14140088+bernovie@users.noreply.github.com>
2018-12-27 01:22:16 +0000

    Adds Image Recognition API to comment based on image content

--- massive_follow_then_unfollow_works-non-stop.py
+++ massive_follow_then_unfollow_works-non-stop.py
@@ -9,6 +9,7 @@ What does this quickstart script aim to do?
 from instapy import InstaPy
 from instapy.util import smart_run
 
+CLARIFAI_API_KEY="8da494ee7d5a4421ab7373e2d3563969"
 
 
 # get a session!
@@ -19,15 +20,29 @@ session = InstaPy(username='clean.em', password='haye12345', disable_image_load=
 with smart_run(session):
     # general settings
     session.set_relationship_bounds(enabled=True, delimit_by_numbers=False, potency_ratio=0.0, max_followers=12000, max_following=4500, min_followers=35, min_following=35)
-    session.set_user_interact(amount=3, randomize=True, percentage=100, media='Photo')
+    session.set_user_interact(amount=2, randomize=True, percentage=100, media='Photo')
     session.set_do_follow(enabled=True, percentage=100)
-    session.set_do_comment(enabled=True, percentage=60)
+    session.set_do_comment(enabled=True, percentage=40)
     session.set_do_like(enabled=True, percentage=100)
-    session.set_comments([u"Beautiful place!😍😍😍 @{}", u"Nice photo!💯💯💯 !@{}", u"You have a nice page!🔥🔥🔥 @{}"], media='Photo')
+    #session.set_comments([u"Beautiful place!😍😍😍 @{}", u"Nice photo!💯💯💯 !@{}", u"You have a nice page!🔥🔥🔥 @{}"], media='Photo')
     session.set_skip_users(skip_no_profile_pic=True,
                        no_profile_pic_percentage=100)
-    session.set_delimit_commenting(enabled=True, comments_mandatory_words=['home','duplex','dreamhome', 'house', 'apartment', 'airbnb', 'condo', 'department','townhome','estate'])
+
+    # Check Description to determine commenting behavior
+    #session.set_delimit_commenting(enabled=True,max=None, min=None, comments_mandatory_words=['home','duplex','dreamhome', 'house', 'apartment', 'airbnb', 'condo', 'department','townhome','estate','space'])
     session.set_mandatory_language(enabled=True, character_set='LATIN')
+    
+    # Use Image detection for comments
+    session.set_use_clarifai(enabled=True, api_key=CLARIFAI_API_KEY, probability=0.72, models=['travel'])
+    session.clarifai_check_img_for(['conference room','internet area','hallway','entrance','ballroom','bar','stairs'], comment=True, comments=[u"Beautiful place!😍😍😍 @{}"])
+    session.clarifai_check_img_for(['reception'], comment=True, comments=[u"Very nice reception!🤩🤩🤩 @{}"])
+    session.clarifai_check_img_for(['lounge','living room'], comment=True, comments=[u"That's a cool lounge!💯💯💯 @{}"])
+    session.clarifai_check_img_for(['lobby'], comment=True, comments=[u"Nice lobby!💯💯💯 @{}"])
+    session.clarifai_check_img_for(['garden'], comment=True, comments=[u"Pretty garden!😍😍😍 @{}"])
+    session.clarifai_check_img_for(['terrace'], comment=True, comments=[u"Pretty terrace!😍😍😍 @{}"])
+    session.clarifai_check_img_for(['food & beverages'], comment=True, comments=["Yummy!", "Tasty!"])
+    session.clarifai_check_img_for(['view'], comment=True, comments=[u"Nice view!🔥🔥🔥"])
+    session.clarifai_check_img_for(['bedroom'], comment=True, comments=[u"Nice bedroom!🤩🤩🤩"])
     #session.set_do_comment(enabled=False, percentage=80)
     #session.set_user_interact(amount=2, randomize=True, percentage=100, media='Photo')
 
@@ -38,6 +53,6 @@ with smart_run(session):
     #session.follow_user_followers(['user1', 'user2', 'user3'], amount=8000, randomize=False, interact=True)
     #session.unfollow_users(amount=1000, nonFollowers=True, style="LIFO", unfollow_after=0, sleep_delay=501)
     #session.unfollow_users(amount=1000, InstapyFollowed=(True, "all"), style="FIFO", unfollow_after=0, sleep_delay=501)
-    session.like_by_tags(['austinrentals'], amount=1000, interact=True)
+    session.like_by_tags(['austinoffices','austinvacationrentals', 'austinofficespace'], amount=500, interact=False)
     #session.like_by_tags(['austinairbnb'], amount=1000)
     #session.unfollow_users(amount=7500, nonFollowers=True, style="RANDOM", unfollow_after=42*60*60, sleep_delay=3)
